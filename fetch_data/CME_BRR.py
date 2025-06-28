20d153c1108f101a79146997729b60074dc45c0a
drxyzw <hirotoo20@gmail.com>
2025-06-28 16:54:59 +0800

    Incremental fetch for BRR, BRRNY, and BRRAP

--- fetch_data/CME_BRR.py
+++ fetch_data/CME_BRR.py
@@ -11,13 +11,25 @@ from tqdm import tqdm
 import time
 import os
 import pandas as pd
+import shutil
 
 import json
 from fetch_data.utils import datetimeToTimestamp, timestampToDatetime
 
-loadDate = dt.now()
-OUTPUT_FILE = "CME_BTC_Future_" + loadDate.isoformat() + ".xlsx"
-OUTPUT_FILE = OUTPUT_FILE.replace(":", "")
+DIR = "./data"
+LATEST_FILE = "CME_BRR_latest.xlsx"
+latest_full_path = DIR + "/" + LATEST_FILE
+existing_dates = []
+df_latest = None
+if os.path.exists(latest_full_path):
+    df_latest = pd.read_excel(latest_full_path)
+    existing_dates_str = df_latest["Date"]
+    existing_dates = pd.to_datetime(existing_dates_str, format="%Y-%m-%dT%H:%M:%S").unique()
+
+loadDate = dt.now().isoformat().replace(":", "")
+loadDate=loadDate[:17] # YYYY-MM-DDTHHMMSS
+OUTPUT_FILE = "CME_BRR_" + loadDate + ".xlsx"
+
 keepOpen = True
 
 url_dict = {
@@ -33,6 +45,9 @@ options.add_experimental_option("detach", False)
 
 driver = webdriver.Chrome(options=options)
 
+result_dfs = [] if df_latest is None else [df_latest]
+
+new_dfs = []
 # get date list
 for indexName, url in url_dict.items():
     driver.get(url)
@@ -41,11 +56,32 @@ for indexName, url in url_dict.items():
     innerJson = json.loads(innerJsonString)
     price_dict_list = innerJson['props']['pageProps']['indexConfig']['rrs']
     price_df = pd.DataFrame(price_dict_list)
-    price_df = price_df[["time", "value"]].copy()
-    price_df['time'] = price_df['time'].map(lambda x: timestampToDatetime(x).isoformat())
+    price_df = price_df.rename(columns={"time": "Date", "value": indexName})
+    price_df = price_df[["Date", indexName]].copy()
+    price_df['Date'] = price_df['Date'].map(lambda x: timestampToDatetime(x).isoformat())
 
-    os.makedirs("./data", exist_ok=True)
-    price_df.to_excel(f"./data/historical_{indexName}.xlsx", index=False)
+    # if price_dfs has element, remove time
+    if len(new_dfs) == 0:
+       new_dfs.append(price_df)
+    else:
+       new_dfs.append(price_df[[indexName]])
+
     print(f"finished {indexName}")
 
-print("Finished all")
+new_df = pd.concat(new_dfs, axis=1)
+new_df['Date'] = pd.to_datetime(new_df.Date)
+new_df = new_df[~new_df.Date.isin(existing_dates)] # remove dates which already exists in previous latest data
+
+if len(new_df) > 0:
+    result_dfs.append(new_df)
+    result_df = pd.concat(result_dfs)
+
+    os.makedirs(DIR, exist_ok=True)
+    result_df.to_excel(DIR + "/" + OUTPUT_FILE, index=False)
+
+    shutil.copy(DIR + "/" + OUTPUT_FILE, latest_full_path)
+
+    print("Finished all and exported to a file")
+else:
+    print("Skipped file export because no new data on website.")
+
