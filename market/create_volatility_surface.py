4ac6bcc28e26be371ad9668545c89fb1d476aad1
drxyzw <hirotoo20@gmail.com>
2025-07-27 02:43:51 +0800

    exporting a list of market objects per date

--- market/create_volatility_surface.py
+++ market/create_volatility_surface.py
@@ -2,12 +2,23 @@ from datetime import datetime as dt
 from market.load_market import loadMarket
 from market.volatility.volatility_surface import *
 
+PROCESSED_DIR = "./data_processed"
     
 if __name__ == "__main__":
     py_date = dt(2025, 6, 13)
     # py_date = dt(2025, 7, 14)
+
+    dfs_mkt_list = []
     while py_date < dt.now():
         market = loadMarket(py_date)
-        market = build_volatility_surface(market)
+        build_volatility_surface(market)
+
+        if not market is None:
+            str_date = py_date.strftime("%Y-%m-%d")
+            df_mkt_list = pd.DataFrame({"Date": [str_date], "MarketObjects": [str(list(market.keys()))]})
+            dfs_mkt_list.append(df_mkt_list)
         py_date += relativedelta(days=1)
 
+    if len(dfs_mkt_list) > 0:
+        df_mkt_list = pd.concat(dfs_mkt_list)
+        df_mkt_list.to_excel(PROCESSED_DIR + "/market_objects.xlsx", index=False)
