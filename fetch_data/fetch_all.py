d5fc6bac19acf3c26bee4d2e6a823b590cbb1f39
drxyzw <hirotoo20@gmail.com>
2025-10-01 20:55:29 +0800

    Removing unnecessary comments

--- fetch_data/fetch_all.py
+++ fetch_data/fetch_all.py
@@ -1,9 +1,9 @@
 import sys, subprocess
 
 subprocess.run([sys.executable, "-m", "fetch_data.SOFR"])
-# subprocess.run([sys.executable, "-m", "fetch_data.Term_SOFR"])
-# subprocess.run([sys.executable, "-m", "fetch_data.CME_SOFR_futures"])
-# subprocess.run([sys.executable, "-m", "fetch_data.CME_SOFR_OIS"])
-# subprocess.run([sys.executable, "-m", "fetch_data.CME_CRYPTO_RR"])
-# subprocess.run([sys.executable, "-m", "fetch_data.CME_CRYPTO_future"])
-# subprocess.run([sys.executable, "-m", "fetch_data.CME_CRYPTO_option"])
+subprocess.run([sys.executable, "-m", "fetch_data.Term_SOFR"])
+subprocess.run([sys.executable, "-m", "fetch_data.CME_SOFR_futures"])
+subprocess.run([sys.executable, "-m", "fetch_data.CME_SOFR_OIS"])
+subprocess.run([sys.executable, "-m", "fetch_data.CME_CRYPTO_RR"])
+subprocess.run([sys.executable, "-m", "fetch_data.CME_CRYPTO_future"])
+subprocess.run([sys.executable, "-m", "fetch_data.CME_CRYPTO_option"])
