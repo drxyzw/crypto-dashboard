4a4b7c797893c22ab57cc43bf2276aaa6e0395d5
drxyzw <hirotoo20@gmail.com>
2025-10-02 04:58:48 +0800

    Console output without buffering

--- fetch_data/fetch_all.py
+++ fetch_data/fetch_all.py
@@ -2,7 +2,7 @@ import sys, subprocess
 
 # subprocess.run([sys.executable, "-m", "fetch_data.SOFR"])
 # subprocess.run([sys.executable, "-m", "fetch_data.Term_SOFR"])
-subprocess.run([sys.executable, "-m", "fetch_data.CME_SOFR_futures"])
+subprocess.run([sys.executable, "-u", "-m", "fetch_data.CME_SOFR_futures"], capture_output=True, text=True)
 # subprocess.run([sys.executable, "-m", "fetch_data.CME_SOFR_OIS"])
 # # subprocess.run([sys.executable, "-m", "fetch_data.CME_CRYPTO_RR"])
 # subprocess.run([sys.executable, "-m", "fetch_data.CME_CRYPTO_future"])
